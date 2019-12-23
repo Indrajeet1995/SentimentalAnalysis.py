acf38b70a054c643d2aca31ff441f79e4c5361f6
Indrajeet1995 <50292904+Indrajeet1995@users.noreply.github.com>
2019-12-23 13:41:26 +0530

    Add files via upload

---
+++ sentiment_analysis.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+from nltk.classify import NaiveBayesClassifier
+ 
+def word_feats(words):
+    return dict([(word, True) for word in words])
+ 
+positive_vocab = [ 'awesome', 'outstanding', 'fantastic', 'terrific', 'good', 'nice', 'great', ':)' ]
+negative_vocab = [ 'bad', 'terrible','useless', 'hate', ':(' ]
+neutral_vocab = [ 'movie','the','sound','was','is','actors','did','know','words','not' ]
+ 
+positive_features = [(word_feats(pos), 'pos') for pos in positive_vocab]
+negative_features = [(word_feats(neg), 'neg') for neg in negative_vocab]
+neutral_features = [(word_feats(neu), 'neu') for neu in neutral_vocab]
+ 
+train_set = negative_features + positive_features + neutral_features
+ 
+classifier = NaiveBayesClassifier.train(train_set) 
+ 
+
+neg = 0
+pos = 0
+sentence = open('movie.txt','r').read()
+
+words = sentence.split(' ')
+for word in words:
+    classResult = classifier.classify( word_feats(word))
+    if classResult == 'neg':
+        neg = neg + 1
+    if classResult == 'pos':
+        pos = pos + 1
+ 
+print('Positive: ' + str(float(pos)/len(words)))
+print('Negative: ' + str(float(neg)/len(words)))
+a= str(float(pos)/len(words))
+b= str(float(neg)/len(words))
+labels = 'Happy', 'Disappointed' 
+sizes = []
+sizes.append(a)
+sizes.append(b)
+colors = ['gold', 'yellowgreen']
+explode = (0.1, 0) 
+ 
+
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+autopct='%1.1f%%', shadow=True, startangle=140)
+ 
+plt.axis('equal')
+plt.show()
